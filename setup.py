df86a9df10c0d48c1d828c9360401f976933dfb5
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2025-01-03 18:28:46 +0100

    Bump version
    Signed-off-by: nutellaBear <48599863+LalithShiyam@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ emails_string = ", ".join([email for name, email in AUTHORS])
 
 setup(
     name='lionz',
-    version='0.11.0',
+    version='0.11.1',
     packages=find_packages(),
     install_requires=[
         'acvl-utils==0.2',
