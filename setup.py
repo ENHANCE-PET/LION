411c76d9fdf0d5bc6feda3e9a28e8b3775f0685e
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2025-01-10 19:11:50 +0100

    Update setup.py
    Signed-off-by: nutellaBear <48599863+LalithShiyam@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ emails_string = ", ".join([email for name, email in AUTHORS])
 
 setup(
     name='lionz',
-    version='0.13.0',
+    version='0.13.1',
     packages=find_packages(),
     install_requires=[
         'acvl-utils==0.2',
